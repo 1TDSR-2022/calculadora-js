c9e68dcabc842654b8f14f25c22965d967cdb142
emersonnfs <rm95199@fiap.com.br>
2022-05-04 23:11:20 -0300

    Sinais adicionados

--- js/script.js
+++ js/script.js
@@ -18,11 +18,31 @@
 const btn1 = document.querySelector('#numero1Id');
 
 
-function adicionaNr(valorBotao){
+//Atrelar ao evento click o método que será executado
+btn1.addEventListener('click', function(event){
     //Recuperando o valor do input
     const elementoResultado = document.querySelector('#frmCalc input');
     //Adicionando o valor do botão ao input
-    elementoResultado.value += valorBotao;
-}
-//Atrelar ao evento click o método que será executado
-btn1.addEventListener('click', adicionaNr('1')); 
+    elementoResultado.value = '1';
+});
+
+//Botão2 adiciona um valor ao input
+const btn2 = document.querySelector('#numero2Id');
+
+btn2.addEventListener('click', function(event){
+    //Recuperando o valor do input
+    const elementoResultado = document.querySelector('#frmCalc input');
+    
+    const operacao = document.querySelector('#operacoesId');
+    if(operacao.value == 'soma'){
+        elementoResultado.value = '2 + ' + elementoResultado.value
+    }else if(operacao.value == 'subtracao'){
+        elementoResultado.value =  '2 - ' + elementoResultado.value
+    }else if(operacao.value == 'multiplicacao'){
+        elementoResultado.value =  '2 x ' + elementoResultado.value
+    }else if(operacao.value == 'divisao'){
+        elementoResultado.value = '2 / ' + elementoResultado.value
+    }
+    
+});   
+ 
