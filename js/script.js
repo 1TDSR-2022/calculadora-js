c8372278b219ccb96daf14ab602866563d36bf69
abner-ferreira <rm93576@fiap.com.br>
2022-05-04 23:14:02 -0300

    Iniciado a calculadora em JS

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+// Diferença entre getElementById e querySelector
+//getElementById retorna um elemento do DOM
+// querySelector retorna um elemento do DOM que corresponde ao seletor CSS
+
+//Recuperando elementos do DOM com getelementById
+// const elementoForm = document.getElementById('frmCalc ');
+// elementoForm.method = 'POST';
+
+// Recuperando elementos do DOM com querySelector
+// const elementoForm = document.querySelector('#frmCalc')
+// console.log(elementoForm.method)
+// Recuperando elementos do DOM com querySelector
+// const elementoResultado = document.querySelector('#frmCalc input')
+// console.log(elementoResultado.value)
+
+// Botão1 - Adicionando um valor ao input 
+const btn1 = document.querySelector('#numero1Id');
+
+//Atrelar ao evento click o método que será executado
+btn1.addEventListener('click', function(evento){
+    //Recuperando o valor de input
+    const elementoResultado = document.querySelector('#frmCalc input');
+    //Adicionando o valor do botão ao input
+    elementoResultado.value += '1';
+});
+
+// Botão2 - Adicionando um valor ao input 
+const btn2 = document.querySelector('#numero2Id');
+
+btn2.addEventListener('click', function(){
+    //Recuperando o valor de input
+    const elementoResultado = document.querySelector('#frmCalc input');
+    const operacao = document.querySelector('#operacoesId')
+    if(operacao.value == 'soma'){
+        elementoResultado.value = '2 + ' + elementoResultado.value
+    }else if (operacao.value == 'subtracao'){
+        elementoResultado.value == '2 - ' + elementoResultado.value
+    }else if (operacao.value == multiplicacao){
+        elementoResultado.value == '2 x ' + elementoResultado.value
+    }else if(operacao.value == 'divisao'){
+        elementoResultado.value == '2 / ' + elementoResultado.value
+    }
+    //Adicionando o valor do botão ao input
+    elementoResultado.value += '2';
+});
+
