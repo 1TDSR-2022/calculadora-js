e869ece89354d46dba168399b836f7dee53e980d
AnaKarolyne <rm93668@fiap.com.br>
2022-05-05 16:21:51 -0300

    adição dos buttons e das operações.

---
+++ js/scrip.js
@@ -0,0 +1,24 @@
+//Diferença entre getElementById e querySelector
+//getElementById retorna um elemento do DOM
+//querySelector retorna um elemento do DOM que corresponde ao seletor CSS
+
+//Recuperando elementos do DOM com getelementById
+//const elementoForm = document.getElementById('frmCalc');
+//elementoForm.method = 'POST';
+
+//Recuperando elementos do DOM com querySelector
+//const elementoForm = document.querySelector('#frmCalc');
+//console.log(elementoForm.method);
+//Recuperando elementos do DOM com querySelector
+//const elementoResultado = document.querySeconst elementoResultado = document.querySelector('#frmCalc input');coconsole.log(elementoResultado.value);
+
+//Botão1 adcionando um valor no input.
+const btn1 = document.querySelector ('#numero1Id');
+
+//Atrelar ao evento click o método que será executado
+btn1.addEventListener('click', function(){
+    //Recuperando o valor do input
+    const elementoResultado = document.querySelector('#frmCalc input');
+    //Adicionando o valor do botão ao input
+    elementoResultado.value += '1';
+});
