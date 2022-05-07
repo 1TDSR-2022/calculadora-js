6b5e21cae0af845ccbc3495265a9f3151025e4d1
jvsobraz <jvsobraz@hotmail.com>
2022-05-06 21:48:39 -0300

    script.js

--- js/script.js
+++ js/script.js
@@ -15,33 +15,39 @@
 //console.log(elementoResultado.value);
  
 //Botão1 adiciona um valor ao input
-const btn1 = document.querySelector('#numero1Id');
+//const btn1 = document.querySelector('#numero1Id');
 
 
 //Atrelar ao evento click o método que será executado
-btn1.addEventListener('click', function(event){
+//btn1.addEventListener('click', function(event){
     //Recuperando o valor do input
-    const elementoResultado = document.querySelector('#frmCalc input');
+    //const elementoResultado = document.querySelector('#frmCalc input');
     //Adicionando o valor do botão ao input
-    elementoResultado.value = '1';
-});
+    //elementoResultado.value = '1';
+//});
 
 //Botão2 adiciona um valor ao input
-const btn2 = document.querySelector('#numero2Id');
+//const btn2 = document.querySelector('#numero2Id');
 
-btn2.addEventListener('click', function(event){
+//btn2.addEventListener('click', function(event){
     //Recuperando o valor do input
-    const elementoResultado = document.querySelector('#frmCalc input');
+    //const //elementoResultado = document.querySelector('#frmCalc input');
     
-    const operacao = document.querySelector('#operacoesId');
-    if(operacao.value == 'soma'){
-        elementoResultado.value = '2 + ' + elementoResultado.value
-    }else if(operacao.value == 'subtracao'){
-        elementoResultado.value =  '2 - ' + elementoResultado.value
-    }else if(operacao.value == 'multiplicacao'){
-        elementoResultado.value =  '2 x ' + elementoResultado.value
-    }else if(operacao.value == 'divisao'){
-        elementoResultado.value = '2 / ' + elementoResultado.value
-    }
+    //const operacao = document.querySelector('#operacoesId');
+    //if(operacao.value == 'soma'){
+        //elementoResultado.value = '2 + ' + elementoResultado.value
+    //}else if(operacao.value == 'subtracao'){
+        //elementoResultado.value =  '2 - ' + elementoResultado.value
+    //}else if(operacao.value == 'multiplicacao'){
+        //elementoResultado.value =  '2 x ' + elementoResultado.value
+    //}else if(operacao.value == 'divisao'){
+        //elementoResultado.value = '2 / ' + elementoResultado.value
+    //}
     
-});   
+//});  
+
+//REcuperar elementos do DOM com querySelector
+//const elemtoForm = document.querySelector("#frmCalc");
+document.querySelector("#numero1Id").addEventListener("click, function(){
+
+})
