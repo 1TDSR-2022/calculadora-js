23274f431f83add1ceb4d12303e9a87df32afb40
Gustavo Meneguesso Costa <RM94838@fiap.com.br>
2022-05-11 22:56:23 -0300

    .

--- js/script.js
+++ js/script.js
@@ -115,3 +115,15 @@ function colocarOperador(valor){
         visor.value += valor;
     }
 }
+
+//Ligar/desligar lampada pelo mouse
+const elementoLigar=document.querySelector("#ligarId")
+const elementoDesligar=document.querySelector("#desligarId")
+
+elementoLigar.addEventListener("click", ()=>{
+    document.querySelector("#imgDesligarId").src="./img/bulb-on.png"
+})
+
+elementoDesligar.addEventListener("click", ()=>{
+    document.querySelector("#imgDesligarId").src="./img/bulb-off.png"
+})
